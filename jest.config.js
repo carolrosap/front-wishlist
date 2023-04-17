ffe607baf6e86146de40a281d6f9a3db2ec99e75
Caroline Rosa <cpauladarosa@gmail.com>
2023-04-17 04:08:11 -0300

    implementing jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    module: 'es6',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest'
+    }
+  };
+  
---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+describe('myModule', () => {
+    test('should do something', () => {
+        // Arrange
+        const value = 42;
+
+        // Act
+        const result = 84;
+
+        // Assert
+        expect(result).toBe(84);
+    });
+});
